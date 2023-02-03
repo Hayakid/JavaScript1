8b8c102679a8ab96b6692b468d1d4cc1033e5b0d
Danil <hisdan56708@gmail.com>
2023-02-03 10:53:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//num1
+let c = 'In degrees Celsius:';
+let far = ' In degrees Farengeits:';
+let formula = 9 / 5
+let degrees = -13
+let num = 32
+console.log()
+console.log(far + (formula + degrees + num))
+console.log(c + degrees)
+//num second
+
+let name = `Danil`
+let admin = name
+console.log(admin)
